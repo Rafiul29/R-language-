4bd25c3fb0943cdf9135622d7fd2e2fdd8167201
Rafiul29 <rafiulislam4472@gmail.com>
2022-09-29 01:48:20 +0600

    strings

---
+++ 03-type_conversion.R
@@ -0,0 +1,9 @@
+# Type conversion
+
+x=10 # numeric vale
+a=as.integer(x)
+print(class(a))
+
+y=20L # integer value
+b=as.numeric(y)
+print(class(b))
---
+++ 04-string.R
@@ -0,0 +1,25 @@
+# String
+str1 = 'statistics graphical, data visualization and data science'
+str2 = 'data analysis and machine learning'
+
+ print(str2)
+ 
+ #length
+ 
+len = nchar(str2)
+print(len)
+ 
+
+#concatenation 
+
+concat = paste(str1,str2)
+print(concat)
+
+#sub string
+substr = substring(str1,1,3)
+print(substr)
+
+# change Case
+
+lower = tolower(str1)
+paste(lower)
