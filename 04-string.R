3386fdeceacee2df58ade8a86a6737bf6b1e0c98
Rafiul29 <rafiulislam4472@gmail.com>
2022-09-29 15:07:14 +0600

    Operator

--- 04-string.R
+++ 04-string.R
@@ -21,5 +21,83 @@ print(substr)
 
 # change Case
 
-lower = tolower(str1)
+lower = tolower(str1) # lower case
 paste(lower)
+
+upper = toupper(str2) #upper case
+print(upper)
+
+#Formation  string conversion
+
+n= 120
+
+strcon = format(n)
+print(strcon)
+
+print(class(strcon))
+
+
+
+n1 =format(2323.23323232,digit=6)
+print(n1)
+
+str="Lorem ipsum dolor sit amet
+consectetur adipiscing elit
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."
+
+
+#Check a String Return a logical value
+grepl("L", str) 
+
+print(str)
+
+
+
+
+# Escape Characters
+
+escapCharacter = "Data Science \'machine learning\' very important part of Statistics"
+
+cat(escapCharacter) 
+
+a <- "Hello"
+b <- 'How'
+c <- "are you? "
+
+print(paste(a,b,c))
+
+print(paste(a,b,c, sep = "-"))
+
+print(paste(a,b,c, sep = " ", collapse = "   "))
+
+result <- format(c(6, 13.14521), scientific = TRUE)
+print(result)
+
+# The minimum number of digits to the right of the decimal point.
+result <- format(23.47, nsmall = 5)
+print(result)
+
+
+
+
+# Left justify strings.
+result <- format("Hello", width = 8, justify = "l")
+print(result)
+
+# Justfy string with center.
+result <- format("Hello", width = 8, justify = "c")
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 05-operator.R
@@ -0,0 +1,82 @@
+#################### Airthmetic Operator
+# +,- *,/,%%,^
+
+#Adds Two Vectors
+v <- c(1,2,3,54,2,3)
+t <- c(0,3,13,4,3,0)
+add = v+t
+print(add)
+
+#Subtraction Two Vector..
+sub = v-t;
+print(sub)
+
+# Division Two Vector...
+div = v/t;
+print(div)
+
+# multiplacation Two Vector 
+mul = v*t
+print(mul)
+
+#remainder two vector ..
+rem = v%%t;
+print(rem);
+
+
+#First vector  raised to the exponent of the second vector
+expon = v^t
+print(expon)
+
+
+############## Realtion operator 
+
+print(v>t)
+print(v<t)
+print(v<=t)
+print(v>=t)
+print(v!=t)
+
+############## Logical Operator 
+###   & | ! Element wise 
+print(v&t)
+print(v|t)
+print(!v)
+print(!t)
+
+############## Logical Operator
+###  && || First Element of both
+
+print(v&&t)
+print(v||t)
+
+
+##################### Asignment Operator 
+# left assignment
+x <- 10
+print(x)
+
+# equal operator
+x= 20
+print(x)
+
+#Right assignment
+15 -> y
+print(y)
+
+#Miscellaneous Operator
+#vector clone
+v = 1:100
+print(v)
+
+#  (%in%) This operator is used to identify if an element belongs to a vector.
+v1 <- 8
+v2 <- 12
+t<- 1:10
+print(v1 %in% t)
+print(v2 %in% t)
+
+# (%*%)  	This operator is used to multiply a matrix with its transpose.
+M <- matrix(c(3:14), nrow = 4, byrow = TRUE)
+print(M)
+
